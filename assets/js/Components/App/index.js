47e710e664194e58b4c77aab46c3dc0c4f37f446
justin-rabiller <justin.rabiller@gmail.com>
2018-09-11 20:05:05 +0200

    Ajout de la structure React

---
+++ assets/js/Components/App/index.js
@@ -0,0 +1,25 @@
+/**
+ * Import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+
+// Styles et assets
+import './app.sass';
+
+/**
+ * Code
+ */
+const App = () => (
+  <div id="app">
+    App
+  </div>
+);
+
+/**
+ * Export
+ */
+export default App;
---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './Components/App';
+
+const rootComponent = (
+  <App />
+);
+
+render(rootComponent, document.getElementById('root'));
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// var $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
