84345f99d62dff6ac99639bb3ad92679b7dba527
q pary <q.pary@outlook.fr>
2018-09-16 13:11:24 +0200

    amélioration requete sql

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Question;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 use Doctrine\ORM\Query\Expr\Join;
@@ -20,25 +21,47 @@ class QuestionRepository extends ServiceEntityRepository
         parent::__construct($registry, Question::class);
     }
 
-
     /**
     * @return Question[] Returns an array of Question objects
     */
-    
     public function findThreeByrandom()
     {
-        return $this->createQueryBuilder('q')
-            // ->andWhere('q.exampleField = :val')
-            // ->innerJoin('q.users', 'u', Join::ON, 'q.id = u.id')
-            // ->innerJoin('q.users', 'u')
-            // ->where('q.id = u.id')
-            ->orderBy('RAND()')
-            ->setMaxResults(3)
-            ->getQuery()
-            ->getResult()  
+        $entityManager = $this->getEntityManager();
+
+        $query = $entityManager->createQuery(
+            'SELECT q.id, q.prop_1, q.prop_2, q.title, u.username, u.email, u.avatar  
+            FROM App\Entity\Question q 
+            INNER JOIN App\Entity\User u
+            WITH q.users = u.id 
+            ORDER BY rand()'
             
-        ;
+    )->setMaxResults(3);
+
+    // returns an array of Product objects
+    return $query->execute(); 
+            
     }
+
+
+    // /**
+    // * @return Question[] Returns an array of Question objects
+    // */
+    
+    // public function findThreeByrandom()
+    // {
+    //     return $this->createQueryBuilder('q')
+    //         // ->andWhere('q.exampleField = :val')
+    //         // ->innerJoin('q.users', 'u', Join::ON, 'q.id = u.id')
+    //         // ->innerJoin('q.users', 'u')
+    //         // ->where('q.id = u.id')
+    //         ->orderBy('RAND()')
+    //         ->setMaxResults(3)
+    //         ->getQuery()
+    //         ->getResult()  
+            
+    //     ;
+    //     // SELECT * FROM question INNER JOIN user ON question.users_id = user.id ORDER BY rand() LIMIT 3;
+    // }
 
 
 //    /**
