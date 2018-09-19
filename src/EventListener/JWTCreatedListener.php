b9764e9fff71835cb0ba767e52c38bc02dc657a7
justin-rabiller <justin.rabiller@gmail.com>
2018-09-19 19:05:32 +0200

    Ajout des datas sécurisé dans le token motherfuckerrr

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,50 @@
+<?php
+// src/App/EventListener/JWTCreatedListener.php
+namespace App\EventListener;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener {
+
+/**
+ * @var RequestStack
+ */
+private $requestStack;
+
+/**
+ * @param RequestStack $requestStack
+ */
+public function __construct(RequestStack $requestStack)
+{
+    $this->requestStack = $requestStack;
+}
+
+/**
+ * @param JWTCreatedEvent $event
+ *
+ * @return void
+ */
+public function onJWTCreated(JWTCreatedEvent $event)
+{
+    $user = $event->getUser();
+
+    $request = $this->requestStack->getCurrentRequest();
+
+    $payload       = $event->getData();
+    $payload['id'] = $user->getId();
+    $payload['username'] = $user->getUsername();
+    $payload['email'] = $user->getEmail();
+    $payload['avatar'] = $user->getAvatar();
+    $payload['birthDate'] = $user->getBirthDate();
+    $payload['score'] = $user->getScore();
+    $payload['departement'] = $user->getDepartements();
+
+    $event->setData($payload);
+    
+    $header        = $event->getHeader();
+    $header['cty'] = 'JWT';
+
+    $event->setHeader($header);
+}
+}
