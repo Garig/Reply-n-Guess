e99a3431aa2b1c73e5e1f34325a39f0581e069bb
justin-rabiller <justin.rabiller@gmail.com>
2018-09-29 19:51:58 +0200

    Add time remaining before closing questions

--- assets/js/Components/DailyQuestions/index.js
+++ assets/js/Components/DailyQuestions/index.js
@@ -3,8 +3,9 @@
  */
 import React from 'react';
 import { Redirect, WithRouter } from 'react-router-dom';
-import { Alert, Card, Icon, Radio } from 'antd';
+import { Alert, Card, Icon, Radio, Tooltip } from 'antd';
 import classNames from 'classnames';
+import moment from 'moment';
 
 /**
  * Local import
@@ -48,72 +49,79 @@ const DailyQuestions = ({ questions, userInterface, setAnswer }) => (
             'predictRight': current.answered === true && current.user_predict === 2
           }
         );
+        var questionsDate = moment(`${current.published_date}`).add(3, 'days');
+        var now = moment();
+        var days = questionsDate.diff(now, 'days');
+        console.log(days);
+        if (days === 0) days = 1;
         return (
-          <Card
-            key={current.id}
-            title={current.title}
-          >
-            <div className="answer">
-              <div className="choice">
-                <Radio.Group
-                  onChange={setAnswer}
-                  name={current.id + '-userChoice'}
-                  buttonStyle="solid">
-                  <Radio.Button
-                    disabled={current.answered}
-                    className={choiceLeft}
-                    value="1">
-                    {current.prop_1}
-                  </Radio.Button>
-                  <Radio.Button value="" disabled>
-                    <Icon type="left" theme="outlined" />
-                    <span className="textVote">Choix</span>
-                    <Icon type="right" theme="outlined" />
-                  </Radio.Button>
-                  <Radio.Button
-                    disabled={current.answered}
-                    className={choiceRight}
-                    value="2">
-                    {current.prop_2}
-                  </Radio.Button>
-                </Radio.Group>
-              </div>
-              {
-                userInterface.isConnected
-                  ? <div className="purpose">
-                    <Radio.Group
-                      onChange={setAnswer}
-                      name={current.id + '-userPredict'}
+          <Tooltip key={current.id} placement="rightTop" title={`Jour restant : ${days}`}>
+            <Card
+              key={current.id}
+              title={current.title}
+            >
+              <div className="answer">
+                <div className="choice">
+                  <Radio.Group
+                    onChange={setAnswer}
+                    name={current.id + '-userChoice'}
+                    buttonStyle="solid">
+                    <Radio.Button
                       disabled={current.answered}
-                      buttonStyle="solid">
-                      <Radio.Button
+                      className={choiceLeft}
+                      value="1">
+                      {current.prop_1}
+                    </Radio.Button>
+                    <Radio.Button value="" disabled>
+                      <Icon type="left" theme="outlined" />
+                      <span className="textVote">Choix</span>
+                      <Icon type="right" theme="outlined" />
+                    </Radio.Button>
+                    <Radio.Button
+                      disabled={current.answered}
+                      className={choiceRight}
+                      value="2">
+                      {current.prop_2}
+                    </Radio.Button>
+                  </Radio.Group>
+                </div>
+                {
+                  userInterface.isConnected
+                    ? <div className="purpose">
+                      <Radio.Group
+                        onChange={setAnswer}
+                        name={current.id + '-userPredict'}
                         disabled={current.answered}
-                        className={predictLeft}
-                        value="1">
-                        {current.prop_1}
-                      </Radio.Button>
-                      <Radio.Button value="" disabled>
-                        <Icon type="left" theme="outlined" />
-                        <span className="textVote">Prédictions</span>
-                        <Icon type="right" theme="outlined" />
-                      </Radio.Button>
-                      <Radio.Button
-                        disabled={current.answered}
-                        className={predictRight}
-                        value="2">
-                        {current.prop_2}
-                      </Radio.Button>
-                    </Radio.Group>
-                  </div>
-                  : null
-              }
-              {
-                current.answered
-                  ? <Alert message="Vous avez répondu à cette question" type="success" showIcon />
-                  : null
-              }
-            </div>
-          </Card>
+                        buttonStyle="solid">
+                        <Radio.Button
+                          disabled={current.answered}
+                          className={predictLeft}
+                          value="1">
+                          {current.prop_1}
+                        </Radio.Button>
+                        <Radio.Button value="" disabled>
+                          <Icon type="left" theme="outlined" />
+                          <span className="textVote">Prédictions</span>
+                          <Icon type="right" theme="outlined" />
+                        </Radio.Button>
+                        <Radio.Button
+                          disabled={current.answered}
+                          className={predictRight}
+                          value="2">
+                          {current.prop_2}
+                        </Radio.Button>
+                      </Radio.Group>
+                    </div>
+                    : null
+                }
+                {
+                  current.answered
+                    ? <Alert message="Vous avez répondu à cette question" type="success" showIcon />
+                    : null
+                }
+              </div>
+            </Card>
+          </Tooltip>
         );
       })
     }
